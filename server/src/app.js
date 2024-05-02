d17be8b437718f24d97c43eb493c9a8c1af07459
khaivern <xarelata@gmail.com>
2024-05-02 22:36:52 +0800

    fix: allow connection from anywhere

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,7 @@ const api = require("./routes/api");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "*" }));
 app.use(morgan("combined"));
 
 app.use(express.json());
